d007a61856e5998eb6d863e7403c51b1939efd89
Paolo Anedda <paolo.anedda@gmail.com>
2021-03-11 12:49:14 +0100

    Some fixture management added in the testing of the Event creation activities

--- src/test/test.js
+++ src/test/test.js
@@ -132,10 +132,12 @@ describe('logbuilder', function() {
   });
 
   describe('eventlog', function() {
+    let parameterList;
+    let logContent;
 
-    it('Trace.getAttributeValuesByName', function() {
-      const parameterList = "timestamp;type;node_description;node_name;carrier_id;sample_id";
-      const logContent = "\"2020-11-27T10:56:45.694Z\";\"dream.uif.tube-log-request\";node_a;a;;\"016405064501\"\n"+
+    beforeEach(function() {
+      parameterList = "timestamp;type;node_description;node_name;carrier_id;sample_id";
+      logContent = "\"2020-11-27T10:56:45.694Z\";\"dream.uif.tube-log-request\";node_a;a;;\"016405064501\"\n"+
       "\"2020-11-27T10:56:46.694Z\";\"dream.uif.tube-log-request\";node_b;b;;\"016405064501\"\n"+
       "\"2020-11-27T10:56:47.694Z\";\"dream.uif.tube-log-request\";node_c;c;;\"016405064501\"\n"+
       "\"2020-11-27T10:56:48.694Z\";\"dream.uif.tube-log-request\";node_d;d;;\"016405064501\"\n"+
@@ -143,6 +145,9 @@ describe('logbuilder', function() {
       "\"2020-11-27T10:56:46.694Z\";\"dream.uif.tube-log-request\";node_b;b;;\"016405064502\"\n"+
       "\"2020-11-27T10:56:47.694Z\";\"dream.uif.tube-log-request\";node_c;c;;\"016405064502\"\n"+
       "\"2020-11-27T10:56:48.694Z\";\"dream.uif.tube-log-request\";node_d;d;;\"016405064502\"\n";
+    });
+
+    it('Trace.getAttributeValuesByName', function() {
       let events = logbuilder.createEventLog(logContent, parameterList);
       let traces = logbuilder.createTraces(events, "sample_id");
       let trace = traces[0].getAttributeValuesByName("node_description");
@@ -154,15 +159,6 @@ describe('logbuilder', function() {
     });
 
     it('Trace.getAttributeSignature', function() {
-      const parameterList = "timestamp;type;node_description;node_name;carrier_id;sample_id";
-      const logContent = "\"2020-11-27T10:56:45.694Z\";\"dream.uif.tube-log-request\";node_a;a;;\"016405064501\"\n"+
-      "\"2020-11-27T10:56:46.694Z\";\"dream.uif.tube-log-request\";node_b;b;;\"016405064501\"\n"+
-      "\"2020-11-27T10:56:47.694Z\";\"dream.uif.tube-log-request\";node_c;c;;\"016405064501\"\n"+
-      "\"2020-11-27T10:56:48.694Z\";\"dream.uif.tube-log-request\";node_d;d;;\"016405064501\"\n"+
-      "\"2020-11-27T10:56:45.694Z\";\"dream.uif.tube-log-request\";node_a;a;;\"016405064502\"\n"+
-      "\"2020-11-27T10:56:46.694Z\";\"dream.uif.tube-log-request\";node_b;b;;\"016405064502\"\n"+
-      "\"2020-11-27T10:56:47.694Z\";\"dream.uif.tube-log-request\";node_c;c;;\"016405064502\"\n"+
-      "\"2020-11-27T10:56:48.694Z\";\"dream.uif.tube-log-request\";node_d;d;;\"016405064502\"";
       let events = logbuilder.createEventLog(logContent, parameterList);
       let traces = logbuilder.createTraces(events, "sample_id");
       let attributeSignature = traces[0].getAttributeSignature("node_description");
